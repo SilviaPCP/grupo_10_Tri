ae965e5c8c0ac77fd369e064ef7d6dc0d8c7f8cf
SilviaPCP <scpc99@hotmail.com>
2021-08-18 20:49:10 -0500

    Iniciando carrito

--- app.js
+++ app.js
@@ -29,3 +29,7 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
+
+app.get('/tienda', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/tienda.html'));
+});
