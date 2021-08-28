29cf11d49f15dc186f42119f12a869f6baf8d163
antoniooroug <antonio.oroug@gmail.com>
2021-08-28 17:43:59 -0500

    Modificaciones al app.js y archivos eliminados

--- app.js
+++ app.js
@@ -1,44 +1,13 @@
 const express = require('express');
-
-//Requerir path
 const path = require('path');
 
-//usar recursos estaticos
-
 const app = express();
-const publicPath = path.resolve(__dirname, './public');
+const mainRutas = require('./routes/main');
 
-app.use(express.static(publicPath));
-app.set("view engine","ejs");
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.use('/', mainRutas);
 
 app.listen(3000, () => {
-    console.log("Servidor corriendo en el puerto 3000")
-});
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './views/home.html'));
-// });
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
-});
-
-app.get('/product-detail', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/product-detail.html'));
-});
-
-app.get('/formulario', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/formulario.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
-});
-
-app.get('/productCart', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
-});
-
-app.get('/productsForm', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productsForm.html'));
+    console.log('Servidor funcionando en el puerto 3000');
 });
