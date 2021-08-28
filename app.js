ffe1ad85208053d068ebfbc80c2ccb9ef37fb614
antoniooroug <antonio.oroug@gmail.com>
2021-08-28 18:12:21 -0500

    Agregando la carpeta src y a su vez añadiendo carpetas dentro

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const mainRutas = require('./routes/mainRouter');
+const mainRutas = require('./src/routes/mainRouter');
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, './src/views'));
 app.set('view engine', 'ejs');
+
 app.use('/', mainRutas);
 
 app.listen(3000, () => {
