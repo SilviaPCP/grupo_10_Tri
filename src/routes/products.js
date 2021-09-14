7a745d7f550cb2f57c1f9389c54aeb7512af1f2f
SilviaPCP <scpc99@hotmail.com>
2021-09-13 21:08:30 -0500

    probando store

--- src/routes/products.js
+++ src/routes/products.js
@@ -8,12 +8,15 @@ const productsController = require('../controllers/productsController');
 
 //CONFIG MULTER//
 const multer = require('multer');
+const { log } = require('console');
+
 const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
-        cb(null, 'public/images/products/');
+        cb(null, path.join(__dirname,'../../public/images/products/'));
     },
     filename: (req, file, cb)=>{
-        cb(null, '${Date.now()+path.extname(file.orginalname)}');
+        const newFilename = Date.now() + path.extname(file.originalname);
+        cb(null, newFilename);
     }
 })
 const upload = multer({storage:storage});
@@ -23,11 +26,11 @@ router.get('/', productsController.index);
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/create/', productsController.create); 
-router.post('/',upload.single('image'), productsController.store); 
+router.post('/store/', upload.single('image'), productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
-router.get('/:id/', productsController.detail); 
+router.get('/detail/:id/', productsController.detail); 
 
 /*** EDIT ONE PRODUCT ***/ 
 router.get('/:id/edit', productsController.edit); 
